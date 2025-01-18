70372fa89b5de7a2e432dabddf1a1962b4d367c9
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-01-18 14:53:48 -0500

    fcc react19 forms section 3 lesson 27 forms:radio / completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ export default function App() {
     const email = formData.get("email")
     const password = formData.get('password')
     const description = formData.get('description')
+    const employmentStatus = formData.get('employmentStatus')
     console.log(email)
     console.log(password)
     console.log(description)
+    console.log(employmentStatus)
   }
   return (
     <>
@@ -31,7 +33,24 @@ export default function App() {
         />
         <br/>
         <label htmlFor='description'>Description:</label>
-        <textarea id='description' name='description'></textarea>
+        <textarea id='description' name='description'></textarea><br/>
+        
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type='radio' name='employmentStatus' value='unemployed' />
+            Unemployed
+          </label>
+          <label>
+            <input type='radio' name='employmentStatus' value='part-time' />
+            Part-time
+          </label>
+          <label>
+            <input type='radio' name='employmentStatus' value='full-time' />
+            Full-time
+          </label>
+        </fieldset>
+
         <button>Submit</button>
       </form>
 
